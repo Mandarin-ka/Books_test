3f1a2c6d03f36d09fb70c5e15127d6163317c865
Evgeniy <mevmrefman@gmail.com>
2024-02-09 14:28:07 +0300

    feat: add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,16 @@ function App() {
   const [request, setRequest] = useState('');
   const [category, setCategory] = useState('all');
   const [sort, setSort] = useState('relevance');
+  const [page, setPage] = useState(0);
 
   return (
     <BrowserRouter>
       <Header category={category} sort={sort} setRequest={setRequest} setCategory={setCategory} setSort={setSort} />
       <Routes>
-        <Route path={'/'} element={<MainPage request={request} category={category} sort={sort} />} />
+        <Route
+          path={'/'}
+          element={<MainPage request={request} category={category} sort={sort} page={page} setPage={setPage} />}
+        />
         <Route path={'/bookPage/:id'} element={<BookPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/utils/UniqData.ts
@@ -0,0 +1,14 @@
+import { IBook } from '../interfaces/IBooks';
+
+export function getUniqData(data: IBook[]) {
+  const ids = [];
+  for (let i = 0; i < data.length; i++) {
+    if (ids.indexOf(data[i].id) !== -1) {
+      data[i] = null;
+    } else {
+      ids.push(data[i].id);
+    }
+  }
+
+  return data.filter((e) => !!e);
+}
