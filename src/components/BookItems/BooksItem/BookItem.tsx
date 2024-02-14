e86c326130fea8e531bac1867c19876fd446a069
Evgeniy <mevmrefman@gmail.com>
2024-02-14 12:56:47 +0300

    add favorites page logic

--- src/components/BookItems/BooksItem/BookItem.tsx
+++ src/components/BookItems/BooksItem/BookItem.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cl from './BookItem.module.css';
 import { Link } from 'react-router-dom';
+import { deleteItem, getItems, setItem } from '../../../utils/localStorage';
 
 function BookItem({ book }: { book: any }) {
+  const [isFavorite, setIsFavorite] = useState(getItems().includes(book.id));
+  const addFavorite = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+
+    if (!isFavorite) {
+      setItem(book.id);
+    } else {
+      deleteItem(book.id);
+    }
+
+    setIsFavorite(!isFavorite);
+  };
+
   return (
     book && (
       <Link to={`bookPage/${book.id}`}>
         <div className={cl.book__item}>
+          <div className={isFavorite ? cl.heart + ' ' + cl.active : cl.heart} onClick={addFavorite}></div>
           <img
             src={book.volumeInfo.imageLinks?.thumbnail || book.volumeInfo.imageLinks?.smallThumbnail}
             alt={book.volumeInfo.title}
