eb0e2afd041b22f750368130b41742f25cc481f7
Evgeniy <mevmrefman@gmail.com>
2024-02-19 13:25:56 +0300

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import './styles/reset.css';
 import './styles/media.css';
 
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { ThemeContext } from './components/Context/ThemeContext';
 import Header from './components/Header/Header';
 import MainRoutes from './components/Routes/MainRoutes';
 import ThemeToggler from './components/UI/ThemeToggler/ThemeToggler';
-import BookPage from './pages/BookPage/BookPage';
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
-import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   const [request, setRequest] = useState('');
