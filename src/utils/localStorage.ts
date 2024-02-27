0ea6538b7e9f0815529820bcb16b6ee4b3f35fdd
Evgeniy <jdev03sgo@gmail.com>
2024-02-27 16:53:10 +0300

    refactor: remove useless logic

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,30 +1,3 @@
-export const setItem = (id: string) => {
-  if (getItems().length) {
-    const books = getItems();
-    books.push(id);
-    localStorage.setItem('books', JSON.stringify([...books]));
-  } else {
-    localStorage.setItem('books', JSON.stringify([id]));
-  }
-};
-
-export const getItems = (): string[] => {
-  if (localStorage.getItem('books') !== null) {
-    return [...JSON.parse(localStorage.getItem('books'))];
-  } else {
-    return [];
-  }
-};
-
-export const deleteItem = (id: string): void => {
-  if (localStorage.getItem('books') !== null) {
-    const books = getItems().filter((e) => e !== id);
-    localStorage.setItem('books', JSON.stringify(books));
-  } else {
-    localStorage.setItem('books', JSON.stringify([]));
-  }
-};
-
 //=====================save themes after reloading
 
 export const setThemeToLS = (theme: 'dark' | 'light'): void => {
