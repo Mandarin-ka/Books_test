ed86d0fb6d5a87e5dfc6a4edeb4c8f8f2218671b
Evgeniy <jdev03sgo@gmail.com>
2024-03-04 15:50:48 +0300

    add load button test

---
+++ src/UI/Button/LoadButton/LoadButton.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import LoadButton from './LoadButton';
+
+describe('load button', () => {
+  test('Render load button', () => {
+    render(<LoadButton>Load more</LoadButton>);
+
+    const button = screen.getByRole('button');
+    expect(button.textContent).toEqual('Load more');
+    button.textContent = 'test';
+    expect(button.textContent).toEqual('test');
+  });
+
+  test('load button theme', () => {
+    const theme = 'dark';
+    render(<LoadButton theme={theme}>Load more</LoadButton>);
+    const button = screen.getByRole('button');
+    expect(button.classList.contains(theme)).toEqual(true);
+  });
+});
